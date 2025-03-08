74b1f0de3e689f66169a02c87f6df666cbd1f238
percymiltier <percymiltier@gmail.com>
2025-03-07 21:52:43 -0500

    Added form page + form template until we have the database setup to use with it

---
+++ viewForms.php
@@ -0,0 +1,119 @@
+<?php
+    // Make session information accessible, allowing us to associate
+    // data with the logged-in user.
+    session_cache_expire(30);
+    session_start();
+
+    $loggedIn = false;
+    $accessLevel = 0;
+    $userID = null;
+    $isAdmin = false;
+    if (!isset($_SESSION['access_level']) || $_SESSION['access_level'] < 1) {
+        header('Location: login.php');
+        die();
+    }
+    if (isset($_SESSION['_id'])) {
+        $loggedIn = true;
+        // 0 = not logged in, 1 = standard user, 2 = manager (Admin), 3 super admin (TBI)
+        $accessLevel = $_SESSION['access_level'];
+        $isAdmin = $accessLevel >= 2;
+        $userID = $_SESSION['_id'];
+    } else {
+        header('Location: login.php');
+        die();
+    }
+    if ($isAdmin && isset($_GET['id'])) {
+        require_once('include/input-validation.php');
+        $args = sanitize($_GET);
+        $id = strtolower($args['id']);
+    } else {
+        $id = $userID;
+    }
+    require_once('database/dbPersons.php');
+    if (isset($_GET['removePic'])) {
+      if ($_GET['removePic'] === 'true') {
+        remove_profile_picture($id);
+      }
+    }
+
+    $user = retrieve_person($id);
+    $viewingOwnForms = $id == $userID;
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php require_once('universal.inc') ?>
+        <link rel="stylesheet" href="css/editprofile.css" type="text/css" />
+        <title>Step VA | View User</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    </head>
+	
+    <body>
+        <?php 
+            require_once('header.php'); 
+            require_once('include/output.php');
+        ?>
+		
+		<?php if ($accessLevel == 0): ?>
+        <h1>Consumer Forms</h1>
+		<?php elseif ($accessLevel == 1): ?>
+        <h1>Volunteer Forms</h1>
+		<?php elseif ($accessLevel == 2 || $accessLevel == 3): ?>
+        <h1>Forms</h1>
+		<?php endif ?>
+				
+        <main class="general">
+            <?php if ($id == 'vmsroots'): ?>
+                <div class="error-toast">The root user cannot submit forms.</div>
+                </main></body></html>
+                <?php die() ?>
+            <?php elseif (!$user): ?>
+                <div class="error-toast">Must be signed in to submit forms.</div>
+                </main></body></html>
+                <?php die() ?>
+            <?php endif ?>
+			
+            <?php if ($viewingOwnForms): ?>
+                <h2>Your Forms</h2>
+            <?php else: ?>
+                <h2>Viewing <?php echo $user->get_first_name() . ' ' . $user->get_last_name() ?>'s Forms</h2>
+            <?php endif ?>
+
+            <fieldset class="section-box">
+				<?php if ($accessLevel == 0): ?>
+				<legend>Consumer Forms</legend>
+				<?php elseif ($accessLevel == 1): ?>
+				<legend>Volunteer Forms</legend>
+				<?php elseif ($accessLevel == 2 || $accessLevel == 3): ?>
+				<legend>Forms</legend>
+				<?php endif ?>
+				
+				<!-- Form links, administrators/superadministrators can see any forms. -->
+				<?php if ($accessLevel == 0 || $accessLevel == 2 || $accessLevel == 3): ?>
+					<!-- Links to consumer forms can be added here -->
+					<div>
+						<div class="field-pair">
+							<a href="">
+								<label>Consumer Form Name</label>
+							</a>
+							<p>Form Description</p>
+						</div>
+					</div>
+				<?php endif ?>
+				
+				<?php if($accessLevel == 1 || $accessLevel == 2 || $accessLevel == 3): ?>
+					<!-- Links to volunteer forms can be added here -->
+					<div>
+						<div class="field-pair">
+							<a href="">
+								<label>Volunteer Form Name</label>
+							</a>
+							<p>Form Description</p>
+						</div>
+					</div>
+				<?php endif ?>
+            </fieldset>
+			<a class="button cancel" href="index.php">Return to Dashboard</a>
+        </main>
+    </body>
+</html>
