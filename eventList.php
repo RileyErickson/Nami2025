7314d71c4008113bab84ea0617855727d5e7c4ed
Marie Satterly <129553120+AmusedC@users.noreply.github.com>
2025-03-16 15:27:53 -0400

    Formatted event hours pages

--- eventList.php
+++ eventList.php
@@ -53,7 +53,6 @@ $event_ids = get_attended_event_ids($username);
 <html lang="en">
 <head>
     <?php require_once('universal.inc'); ?>
-    <link rel="stylesheet" href="css/editprofile.css" type="text/css" />
     <title>NAMI Rappahannock | User Events</title>
 </head>
 <body>
