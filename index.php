1afd6efb940367d0dcf03ec8e55c6330899a1822
mcrimUMW <mcrim@umw.edu>
2025-04-16 09:29:43 -0400

    Added Hours button to volunteer side.
    Adjust from Delete hours to Mange Hours.
    Added permission to hours.php

--- index.php
+++ index.php
@@ -249,7 +249,7 @@
 <!--                --><?php //endif ?>
 
                 <!-- FOR BOARD ONLY -->
-                <?php elseif ($_SESSION['access_level'] >= 4) : ?>
+                <?php elseif ($_SESSION['access_level'] >= 2) : ?>
 
                     <main class='dashboard'>
                         <div id="dashboard">
@@ -301,7 +301,21 @@
 
                 <?php elseif ($notRoot) : ?>
                     <main class='dashboard'>
+
+                        <div class="top-bar">
+
+                            <a href="hours.php" class="clickable-box">
+                                <div class="highlight">
+                                    <span class="box-text">Hours Management</span>
+                                    <img src="images/highlight_background.jpg" alt="highlight" />
+                                </div>
+                            </a>
+
+                        </div>
+
                         <div id="dashboard">
+
+
                             <div class="dashboard-item" data-link="viewForms.php">
                                 <img src="images/viewforms.png">
                                 <span><center>View Forms</center></span>
@@ -323,10 +337,10 @@
                             <!--                                <i class="fa-solid fa-list"></i>-->
                             <!--                                <span><center>My Upcoming Events</center></span>-->
                             <!--                            </div>-->
-                            <div class="dashboard-item" data-link="volunteerReport.php">
-                                <img src="images/volunteer-history.svg">
-                                <span><center>View Volunteering Report</center></span>
-                            </div>
+<!--                            <div class="dashboard-item" data-link="volunteerReport.php">-->
+<!--                                <img src="images/volunteer-history.svg">-->
+<!--                                <span><center>View Volunteering Report</center></span>-->
+<!--                            </div>-->
                             <!--                            <div class="dashboard-item" data-link="editHours.php">-->
                             <!--                                <img src="images/add-person.svg">-->
                             <!--                                <span><center>View & Change My Event Hours</center></span>-->
