d9f2f3c99560216872a91a35be3baddf97bb1f42
Peter <petertannisiii@gmail.com>
2025-04-29 02:51:29 -0400

    putting dbinfo back how i found it

--- database/dbinfo.php
+++ database/dbinfo.php
@@ -19,11 +19,9 @@ function connect() {
     $host = "localhost"; 
     $database = "dbahtngmaidjgd";
 
-    $user = "stepvadb";
-    $pass = "stepvadb";
-// might need to change to this when merging
-    //$user = "root";
-    //$pass = "";
+
+    $user = "root";
+    $pass = "";
 
     if ($_SERVER['SERVER_NAME'] == 'jenniferp161.sg-host.com') {
         $user = 'uhxi7ly4lmfqq';
